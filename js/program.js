2ca65e28608273a01d4a591e2c543f3be4f3c979
Olivier Barais <olivieR.barais@gmail.com>
2021-04-07 22:42:29 +0200

    update for private nav

--- js/program.js
+++ js/program.js
@@ -1,8 +1,10 @@
 var converter = new showdown.Converter();
 var modelsApp = angular.module("models-app", []);
-modelsApp.filter('unsafe', function($sce) { return $sce.trustAsHtml; });
+modelsApp.filter('unsafe', function ($sce) {
+    return $sce.trustAsHtml;
+});
 
-modelsApp.controller("ProgramController", function($scope, $window) {
+modelsApp.controller("ProgramController", function ($scope, $window) {
 
     // Utils
     function parseTime(time) {
@@ -13,13 +15,13 @@ modelsApp.controller("ProgramController", function($scope, $window) {
         }
     }
 
-    $scope.getStartOfSessionGroup = function(sessionGroup) {
+    $scope.getStartOfSessionGroup = function (sessionGroup) {
         return sessionGroup[0].start;
     };
 
-    $scope.getEndOfSessionGroup = function(sessionGroup) {
+    $scope.getEndOfSessionGroup = function (sessionGroup) {
         var maxEnd = "00:00";
-        sessionGroup.forEach(function(session) {
+        sessionGroup.forEach(function (session) {
             if (typeof session.end !== "undefined") {
                 var parsedSessionEnd = parseTime(session.end);
                 var parsedMaxEnd = parseTime(maxEnd);
@@ -34,47 +36,57 @@ modelsApp.controller("ProgramController", function($scope, $window) {
 
     var hiddenTypes = ["Poster", "SRC", "DoctoralSymposium", "Clinic", "EducatorSymposium", "Reception", "Opening"];
 
-    $scope.hideType = function(talkType) {
+    $scope.hideType = function (talkType) {
         return typeof talkType === "undefined" || hiddenTypes.indexOf(talkType) !== -1
     };
 
     var hiddenModalsAndStars = ["Lunch", "CoffeeBreak", "Reception"];
-    $scope.showModalAndStar = function(talkType) {
-      return typeof talkType !== "undefined" && hiddenModalsAndStars.indexOf(talkType) === -1
+    $scope.showModalAndStar = function (talkType) {
+        return typeof talkType !== "undefined" && hiddenModalsAndStars.indexOf(talkType) === -1
     };
 
-    
+
 
     ////// Preprocess data //////
 
     $scope.data = data;
 
     ////// Favorites /////
-    if (typeof localStorage !== 'undefined'){
-        $scope.showFavorites = localStorage.getItem("showFavorites") === "true";
-    } else {
-        $scope.showFavorites = false
-    }
 
-    // Retrieve favorite talks from local storage
-    if(typeof(Storage) !== "undefined" && typeof localStorage !== 'undefined') {
-        $scope.favoriteTalks = JSON.parse(localStorage.getItem("favoriteTalks"));
-        if ($scope.favoriteTalks === null) {
+    try {
+
+        if (typeof localStorage !== 'undefined') {
+            $scope.showFavorites = localStorage.getItem("showFavorites") === "true";
+        } else {
+            $scope.showFavorites = false
+        }
+
+        // Retrieve favorite talks from local storage
+        if (typeof (Storage) !== "undefined" && typeof localStorage !== 'undefined') {
+            $scope.favoriteTalks = JSON.parse(localStorage.getItem("favoriteTalks"));
+            if ($scope.favoriteTalks === null) {
+                $scope.favoriteTalks = {};
+            }
+        } else {
             $scope.favoriteTalks = {};
         }
-    } else {
+    } catch (error) {
+        //console.error(error);
+        // expected output: ReferenceError: nonExistentFunction is not defined
+        // Note - error messages will vary depending on browser
         $scope.favoriteTalks = {};
     }
 
-    $scope.data.forEach(function(day) {
-        day.sessionGroups.forEach(function(sessionGroup) {
+
+    $scope.data.forEach(function (day) {
+        day.sessionGroups.forEach(function (sessionGroup) {
             sessionGroup.forEach(function (session, roomIndex) {
                 if (typeof session.events !== "undefined") {
-                    session.events.forEach(function(event) {
+                    session.events.forEach(function (event) {
                         if (typeof event.papers === "undefined") {
                             event.selected = $scope.favoriteTalks[event.title + session.icalStart];
                         } else {
-                            event.papers.forEach(function(talk) {
+                            event.papers.forEach(function (talk) {
                                 talk.selected = $scope.favoriteTalks[talk.title + talk.icalStart];
                             });
                         }
@@ -84,25 +96,32 @@ modelsApp.controller("ProgramController", function($scope, $window) {
         });
     });
 
-    $scope.toggleFavorites = function() {
-        if (typeof localStorage !== 'undefined'){
-            localStorage.setItem("showFavorites", $scope.showFavorites);
+    $scope.toggleFavorites = function () {
+        try {
+            if (typeof localStorage !== 'undefined') {
+                localStorage.setItem("showFavorites", $scope.showFavorites);
+            }
+        } catch (error) {
+
         }
     };
 
-    $scope.toggleFavoriteTalk = function(talk, date) {
-        talk.selected=!talk.selected;
+    $scope.toggleFavoriteTalk = function (talk, date) {
+        talk.selected = !talk.selected;
         $scope.favoriteTalks[talk.title + date] = talk.selected;
+        try {
+            if (typeof (Storage) !== "undefined" && typeof localStorage !== 'undefined') {
+                localStorage.setItem("favoriteTalks", JSON.stringify($scope.favoriteTalks));
+            }
+        } catch (error) {
 
-        if(typeof(Storage) !== "undefined" && typeof localStorage !== 'undefined') {
-            localStorage.setItem("favoriteTalks", JSON.stringify($scope.favoriteTalks));
         }
     };
 
-    $scope.showColor = function(event) {
+    $scope.showColor = function (event) {
         var atLeastOneSelected = false;
         if (typeof event.papers !== 'undefined') {
-            event.papers.forEach(function(talk) {
+            event.papers.forEach(function (talk) {
                 atLeastOneSelected = atLeastOneSelected || talk.selected;
             });
         } else {
@@ -120,7 +139,7 @@ modelsApp.controller("ProgramController", function($scope, $window) {
         if (string.length == 0) return hash;
         for (i = 0; i < string.length; i++) {
             var charI = string.charCodeAt(i);
-            hash = ((hash<<5)-hash)+charI;
+            hash = ((hash << 5) - hash) + charI;
             hash = hash & hash;
         }
         return hash;
@@ -129,21 +148,21 @@ modelsApp.controller("ProgramController", function($scope, $window) {
 
     function createEvent(calendar, edate, start, end, title, description, location) {
         calendar.push("BEGIN:VEVENT");
-//        calendar.push("DTSTART;TZID=Europe/Paris:" + toITCFormat(edate,start));
-        calendar.push("DTSTART:" + toITCFormat(edate,start));
-        calendar.push("DTEND:" + toITCFormat(edate,end));
-        calendar.push("DTSTAMP:" + toITCFormat(edate,start));
-//        calendar.push("DTEND;TZID=Europe/Paris:" + toITCFormat(edate,end));
-//        calendar.push("DTSTAMP;TZID=Europe/Paris:" + toITCFormat(edate,start));
+        //        calendar.push("DTSTART;TZID=Europe/Paris:" + toITCFormat(edate,start));
+        calendar.push("DTSTART:" + toITCFormat(edate, start));
+        calendar.push("DTEND:" + toITCFormat(edate, end));
+        calendar.push("DTSTAMP:" + toITCFormat(edate, start));
+        //        calendar.push("DTEND;TZID=Europe/Paris:" + toITCFormat(edate,end));
+        //        calendar.push("DTSTAMP;TZID=Europe/Paris:" + toITCFormat(edate,start));
         calendar.push("ORGANIZER;CN=icpe2021-gc@inria.fr:mailto:icpe2021-gc@inria.fr");
-        calendar.push("UID:" + toITCFormat(edate,end) + "-"  + hash(title) + "@icpe2021.irisa.fr");
+        calendar.push("UID:" + toITCFormat(edate, end) + "-" + hash(title) + "@icpe2021.irisa.fr");
         calendar.push("DESCRIPTION:" + description); // TODO : max line is 75 characters
         calendar.push("LOCATION:" + location);
         calendar.push("SUMMARY:" + title); // TODO : max line is 75 characters
         calendar.push("END:VEVENT");
     }
 
-    $scope.exportToCal = function(favoritesOnly) {
+    $scope.exportToCal = function (favoritesOnly) {
 
         // Create calendar
         var calendar = [];
@@ -151,51 +170,51 @@ modelsApp.controller("ProgramController", function($scope, $window) {
         calendar.push("VERSION:2.0");
         calendar.push("PRODID:-//ICPE2021//Program");
 
-        $scope.data.forEach(function(day) {
+        $scope.data.forEach(function (day) {
 
-           day.sessionGroups.forEach(function(sessionGroup) {
-               if (sessionGroup.length > 0) {
-                   sessionGroup.forEach(function (session, roomIndex) {
-                       if (typeof session.events !== "undefined") {
-                           session.events.forEach(function (event, eventIndex) {
+            day.sessionGroups.forEach(function (sessionGroup) {
+                if (sessionGroup.length > 0) {
+                    sessionGroup.forEach(function (session, roomIndex) {
+                        if (typeof session.events !== "undefined") {
+                            session.events.forEach(function (event, eventIndex) {
 
-                               if (typeof event.papers === "undefined") {
+                                if (typeof event.papers === "undefined") {
 
-                                   if (!favoritesOnly || ((typeof event.selected !== "undefined") && event.selected === true)) {
+                                    if (!favoritesOnly || ((typeof event.selected !== "undefined") && event.selected === true)) {
                                         createEvent(calendar, session.date, session.start, session.end, event.title, event.title, session.room); // TODO : description
-                                   }
-                               } else {
-                                   event.papers.forEach(function(talk, talkIndex) {
-                                       if (!favoritesOnly || ((typeof talk.selected !== "undefined") && talk.selected === true)) {
-                                           if (talk.date == undefined || talk.start == undefined || talk.end == undefined ){
-                                            createEvent(calendar,  session.date, session.start, session.end, talk.title, talk.title, session.room); // TODO : description
-                                           }
-                                           else{
-                                            createEvent(calendar,  talk.date, talk.start, talk.end, talk.title, talk.title, session.room); // TODO : description
+                                    }
+                                } else {
+                                    event.papers.forEach(function (talk, talkIndex) {
+                                        if (!favoritesOnly || ((typeof talk.selected !== "undefined") && talk.selected === true)) {
+                                            if (talk.date == undefined || talk.start == undefined || talk.end == undefined) {
+                                                createEvent(calendar, session.date, session.start, session.end, talk.title, talk.title, session.room); // TODO : description
+                                            } else {
+                                                createEvent(calendar, talk.date, talk.start, talk.end, talk.title, talk.title, session.room); // TODO : description
+                                            }
                                         }
-                                       }
-                                   });
-                               }
+                                    });
+                                }
 
 
-                           });
-                       }
-                   });
-               }
-           });
+                            });
+                        }
+                    });
+                }
+            });
         });
 
         calendar.push("END:VCALENDAR");
         var file = calendar.join("\n");
 
         // Download file
-        var blob = new Blob([file], {type: 'text/calendar'});
+        var blob = new Blob([file], {
+            type: 'text/calendar'
+        });
         var inlinedDataUrl = 'data:text/calendar;charset=utf-8,' + encodeURIComponent(file);
         var filename = "icpe2021.ics";
-        if(window.navigator.msSaveOrOpenBlob) {
+        if (window.navigator.msSaveOrOpenBlob) {
             window.navigator.msSaveBlob(blob, filename);
-        }
-        else{
+        } else {
             var elem = window.document.createElement('a');
 
             if (window.URL) {
@@ -219,30 +238,30 @@ modelsApp.controller("ProgramController", function($scope, $window) {
     };
 
     ///// Info on talk /////
-    $scope.getInfo = function(event, talk, date) {
+    $scope.getInfo = function (event, talk, date) {
         y = event.pageY;
         $scope.selectedTalk = talk;
         $scope.selectedTalkDate = date;
     }
-    $scope.convertHtml = function(desc) {
+    $scope.convertHtml = function (desc) {
 
         return converter.makeHtml('**Description**:' + desc);
     }
-    $scope.convertHtmlprogram = function(desc) {
+    $scope.convertHtmlprogram = function (desc) {
 
         return converter.makeHtml('**Program**:' + desc);
     }
 
 });
 
-var y=1;
+var y = 1;
 
 $('#infoModal').on('show.bs.modal', function (e) {
-    $('#infoModal').css('top', y+'px');
+    $('#infoModal').css('top', y + 'px');
 });
 
 $('#infoModal').on('hide.bs.modal', function (e) {
-//    y = 1
+    //    y = 1
 });
 
 function toITCFormat(date, time) {
@@ -253,25 +272,25 @@ function toITCFormat(date, time) {
 
         timeCont = time.toLowerCase().replace('pm', 00).split(':'); //assuming from your question seconds is never mentioned but only hh:mm i.e. hours and minutes
         timeCont[0] = (parseInt(timeCont[0]) + 12) % 24;
-    } else     if (time.toLowerCase().indexOf('am') != -1) {
+    } else if (time.toLowerCase().indexOf('am') != -1) {
         timeCont = time.toLowerCase().replace('am', 00).split(':');
-    }else{
-        timeCont = (time.toLowerCase() +  '00').split(':');
+    } else {
+        timeCont = (time.toLowerCase() + '00').split(':');
     }
     dateCont = date.split('/');
 
-    return dateCont.join('') + 'T' + (timeCont[0]-2) + timeCont[1]  +'Z';
+    return dateCont.join('') + 'T' + (timeCont[0] - 2) + timeCont[1] + 'Z';
 }
 
 
 //var x = toITCFormat('2014/09/04', '02:30PM');
 //console.log(x); // this will output ur .ics format
 
-window.addEventListener('message', function(event) {
+window.addEventListener('message', function (event) {
     var messageContent = event.data.split(':');
     var topOffset = messageContent[0];
     var currentScroll = messageContent[1];
     y = currentScroll
     //calculate padding value and update the modal top-padding
-  
-  }, false);
+
+}, false);
